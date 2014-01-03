9ee5dd3c08d9fbb3f8edecd386458e7d1db8702a
Parinda Darden <parinda.darden@gmail.com>
2014-01-02 20:31:57 -0500

    Category model, seeder, and rspec

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Seeders::Categories.seed
+Seeders::Brands.seed
+Seeders::RidingStyles.seed
