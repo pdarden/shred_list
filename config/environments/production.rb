eb24d88250e34f03bb28f049c82806a2f0f633ae
Parinda Darden <parinda.darden@gmail.com>
2014-01-15 23:39:45 -0500

    Config for devise in different environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ ShredList::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { :host => 'www.shred-list.com' }
 end
