1030f3fe2a23522b6d228b7376f365f1c657b622
Parinda Darden <parinda.darden@gmail.com>
2014-01-22 14:19:25 -0500

    Map shows number of listings and is clickable

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,7 @@
 class MapsController < ApplicationController
   def index
+    @states = {}
+    State.pluck(:id, :name).each{|m| @states[m[1]] = m[0]}
+    @counts = Listing.joins(:state).group(:name).count
   end
 end
